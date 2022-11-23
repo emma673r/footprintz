414111e43111e262acf710ea74e58d80bc5c7e45
emma673r <90177139+emma673r@users.noreply.github.com>
2022-11-23 14:03:38 +0100

    sass skeleton done

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "../styles/header.module.scss";
 
+//!! all inputs written like this:
+//TODO  <input type="text" pattern="[0-999]+" />
+
 function Layout({ children }) {
   return (
     <>
-      <header className={styles.sm}>
-        <h1 className={styles.hine}>FOOTPRINTZ</h1>
+      <header>
+        <h1>FOOTPRINTZ</h1>
         <nav></nav>
       </header>
       <main>{children}</main>
