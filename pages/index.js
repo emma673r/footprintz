9e9698058516041b4a6c99da49f8aabe885ff4aa
emma673r <90177139+emma673r@users.noreply.github.com>
2022-11-24 00:11:00 +0100

    if it works it works -- index.js + somebutton

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,43 @@ import Head from "next/head";
 import IndexIntroText from "../components/IndexIntroText";
 import StreamButton from "../components/StreamButton";
 import SomeButton from "../components/SomeButton";
+import SomeForm from "../components/SomeForm";
+import { useState } from "react";
+
 export default function Home() {
+  const [showSomeForm, setShowSomeForm] = useState(false);
+
+  const [data, setData] = useState("");
+
+  const childToParent = (data) => {
+    console.log(data);
+    setData(data);
+    setShowSomeForm(data);
+  };
+
   return (
     <>
+      {/* {!showSomeForm && <button onClick={() => setShowSomeForm(true)}>How much social media do I use?</button>} */}
+      {!showSomeForm && (
+        <>
+          <IndexIntroText />
+          <div className="s-buttons">
+            <StreamButton />
+            <SomeButton childToParent={childToParent} />
+          </div>
+        </>
+      )}
+      {showSomeForm && <SomeForm />}
+
       <Head>
         <title>FOOTPRINTZ</title>
       </Head>
-      <main>
-        <IndexIntroText />
-        <div className="s-buttons">
-          <StreamButton />
-          <SomeButton />
-        </div>
-      </main>
+
+      {/* <IndexIntroText />
+      <div className="s-buttons">
+        <StreamButton />
+        <SomeButton onClick={() => setShowSomeForm(true)} />
+      </div> */}
     </>
   );
 }
